089a4bbcc317e894dbd1f1e1e3a8160d9a14d5f4
isafo <isabelle.forsman@gmail.com>
2015-09-07 14:57:25 +0200

    done task 1-5

--- Lab0/Vec3f.cpp
+++ Lab0/Vec3f.cpp
@@ -1,9 +1,64 @@
-
-
 #include "Vec3f.h"
 
+#include <algorithm>    // std::copy
 
+Vec3f::Vec3f(){
+	value[0] = 0;
+	value[1] = 0;
+	value[2] = 0; 
+};
 
+Vec3f::Vec3f(float x, float y, float z){
+	value[0] = x;
+	value[1] = y;
+	value[2] = z;
+}
 
+Vec3f& Vec3f::operator= (const Vec3f &other) {
+	if (this != &other) {
+		std::copy(other.value, other.value + 3, this->value);
+	}
+	return *this;
+}
 
+Vec3f& Vec3f::operator+ (const Vec3f &other) {
 
+	for (int i = 0; i < 3; i++) {
+		this->value[i] += other.value[i];
+	}
+	return *this;
+}
+
+Vec3f& Vec3f::operator- (const Vec3f &other) {
+
+	for (int i = 0; i < 3; i++) {
+		this->value[i] -= other.value[i];
+	}
+	return *this;
+}
+
+float Vec3f::operator* (const Vec3f &other) {
+	float dotProd;
+	
+	for (int i = 0; i < 3; i++) {
+		dotProd += this->value[i] * other.value[i];
+	}
+	return dotProd;
+}
+
+Vec3f& Vec3f::operator% (const Vec3f &other) {
+	Vec3f newVec;
+
+	newVec.value[0] = this->value[1] * other.value[2] - this->value[2] * other.value[1];
+	newVec.value[1] = this->value[0] * other.value[2] - this->value[2] * other.value[0];
+	newVec.value[2] = this->value[0] * other.value[1] - this->value[1] * other.value[0];
+
+	return newVec;
+}
+
+Vec3f operator*(float i, Vec3f v) {
+	for (int i = 0; i < 3; i++) {
+		v.value[i] = v.value[i] * i;
+	}
+	return v;
+}
--- Lab0/Vec3f.h
+++ Lab0/Vec3f.h
@@ -1,3 +1,25 @@
 
 template<typename T>
-class Vec3 {};
+class Vec3 {
+};
+
+
+class Vec3f {
+
+friend Vec3f operator*(float i, Vec3f v);
+
+public:
+	Vec3f();
+	Vec3f(float x, float y, float z);
+
+	Vec3f& operator= (const Vec3f &other);
+	Vec3f& operator+ (const Vec3f &other);
+	Vec3f& operator- (const Vec3f &other);
+	float operator* (const Vec3f &other);
+	Vec3f& operator% (const Vec3f &other);
+
+protected:
+	float value[3];
+};
+
+Vec3f operator*(float i, Vec3f v);
